4ce4194d0f87a559632089e231131672730169a6
山猫 <lynx.kor@163.com>
2019-06-01 16:50:14 +0800

    provider

---
+++ lce.mis.Provider/OrderParam.cs
@@ -0,0 +1,26 @@
+﻿namespace lce.mis.api.Provider
+{
+    /// <summary>
+    /// 排序
+    /// </summary>
+    public class OrderParam
+    {
+        public OrderParam()
+        {
+        }
+
+        public OrderParam(string field) : this()
+        {
+            this.Field = field;
+        }
+
+        public OrderParam(string field, bool isAsc) : this(field)
+        {
+            this.IsAsc = isAsc;
+        }
+
+        public string Field { get; set; } = "Id";
+
+        public bool IsAsc { get; set; } = false;
+    }
+}
