34c94947b32f0bcd0a803c6e79d6f3e7c7bcc026
山猫 <lynx.kor@163.com>
2019-06-01 16:43:55 +0800

    factories

---
+++ lce.mis.api.Factories/MisDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace lce.mis.api.Factories
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class MisDbContext : DbContext
+    {
+        public MisDbContext(DbContextOptions<MisDbContext> options) : base(options) { }
+    }
+}
